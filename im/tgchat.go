56750cd31d47b83538e08b99fe5e0a75265e24fc
DiamondGo <kenxieca@gmail.com>
2023-03-12 18:08:00 -0700

    markdown escape

--- im/tgchat.go
+++ im/tgchat.go
@@ -226,7 +226,9 @@ func escapeSafeForMarkdown(s string) string {
 	s = strings.ReplaceAll(s, "!", `\!`)
 	s = strings.ReplaceAll(s, ".", `\.`)
 	s = strings.ReplaceAll(s, "(", `\(`)
+	s = strings.ReplaceAll(s, ")", `\)`)
 	s = strings.ReplaceAll(s, "-", `\-`)
+	s = strings.ReplaceAll(s, "+", `\+`)
 
 	return s
 }
